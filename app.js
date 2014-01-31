a109aa4465441c1f8995c8bde132ad8b1f01b0a6
Kevin Lee <kevinleeone@gmail.com>
2014-01-31 21:29:37 +0800

    [master] Set logger

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var http = require('http');
+var path = require('path');
 
 var app = express();
 app.set('port', 8000);
+app.use(express.logger('dev'));
 app.use(express.static(__dirname));
 http.createServer(app).listen(app.get('port'), function() {
-  console.log('Started');
+  console.log('Express server listening on port ' + app.get('port'));
 });
